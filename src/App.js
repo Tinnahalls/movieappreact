37d18fb4078f34e220a445defbe86b4a1ee93b4a
Tinnahalls <tinnahalls@gmail.com>
2021-12-11 18:47:39 +0000

    commit all

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React, { useState, useEffect } from 'react'
+import Header from './componenent/UI/Header'
+import CharacterGrid from './componenent/characters/CharacterGrid'
+import Search from './componenent/UI/Search'
+
+function App() {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+  const axios = require('axios');
+  
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      //console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+
+  return (
+    <div className="container">
+      <Header /> 
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading}  items={items} />
+
+    </div>
+  );
+}
+
+export default App;
